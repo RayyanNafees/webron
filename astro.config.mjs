5ba6911c636b5fed52fafb8a7f8140023e5f7dda
Rayyan Nafees <59685062+RayyanNafees@users.noreply.github.com>
2023-05-29 13:34:46 +0000

    astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
+import UnoCSS from 'unocss/astro';
 import sitemap from '@astrojs/sitemap';
+
+import vercel from "@astrojs/vercel/serverless";
+import solidJs from "@astrojs/solid-js";
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), UnoCSS(), solidJs(), image()],
+  output: "server",
+  adapter: vercel()
 });
